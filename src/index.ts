6f0cbff436b450cc3ccc1c2c9c8877e2a2b4c397
gabrielbmoro <gabriel.moro@mindsea.com>
2021-03-22 14:13:48 -0300

    Adding swagger to the project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { ErrorHandler } from "./errors/error.handling";
 import { usersRoutes } from "./routes/users.routes";
+import * as swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const errorHandler = new ErrorHandler();
 
